c3ac74781ea87a921c125b31852fecfd553833d7
DooGles <haxingrulez.yt@gmail.com>
2023-02-24 17:21:56 +0800

    Make a change on txtbox that will use got API.

--- CG MES - MTF Creation Module/Form/frmMain.Designer.vb
+++ CG MES - MTF Creation Module/Form/frmMain.Designer.vb
@@ -331,6 +331,7 @@ Partial Class frmMain
         Me.txtJsonPN.TabIndex = 12
         Me.txtJsonPN.Text = "{""pn"":""[PART-NUM]"",""qty"":""[PART-QTY]"",""dsc"":"""",""out_wh"":"""",""group_id"":"""",""priorit" &
     "y"":""0"",""item"":""[ITEM-NUM]""}"
+        Me.txtJsonPN.Visible = False
         '
         'txtResult
         '
@@ -339,6 +340,7 @@ Partial Class frmMain
         Me.txtResult.Name = "txtResult"
         Me.txtResult.Size = New System.Drawing.Size(484, 265)
         Me.txtResult.TabIndex = 11
+        Me.txtResult.Visible = False
         '
         'txtJsonHeader
         '
@@ -348,6 +350,7 @@ Partial Class frmMain
         Me.txtJsonHeader.Size = New System.Drawing.Size(484, 265)
         Me.txtJsonHeader.TabIndex = 10
         Me.txtJsonHeader.Text = resources.GetString("txtJsonHeader.Text")
+        Me.txtJsonHeader.Visible = False
         '
         'TextBox1
         '
@@ -356,6 +359,7 @@ Partial Class frmMain
         Me.TextBox1.Name = "TextBox1"
         Me.TextBox1.Size = New System.Drawing.Size(484, 119)
         Me.TextBox1.TabIndex = 13
+        Me.TextBox1.Visible = False
         '
         'frmMain
         '
